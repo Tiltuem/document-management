b773e66272433ab75e533a11c1db24d32b913170
Kirill <dekerilljr@gmail.com>
2024-05-08 18:32:48 +0400

    fix: changed models

--- src/main/java/com/java/ponomarenko/model/Document.java
+++ src/main/java/com/java/ponomarenko/model/Document.java
@@ -1,36 +1,30 @@
 package com.java.ponomarenko.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedBy;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
-@MappedSuperclass
+@Entity
 @EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
-public abstract class Document {
+public class Document {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String file;
     private String city;
-
     @Column(columnDefinition = "date")
     @CreatedDate
     private Date createdDate;
+    private Date endDate;
+    @Enumerated(EnumType.STRING)
+    private DocumentType type;
 
-    @Column(columnDefinition = "date")
-    @LastModifiedDate
-    private Date modifiedDate;
-
-    @CreatedBy
-    private String createdBy;
-
-    @LastModifiedBy
-    private String modifiedBy;
 }
---
+++ src/main/java/com/java/ponomarenko/model/InnerType.java
@@ -0,0 +1,4 @@
+package com.java.ponomarenko.model;
+
+public interface InnerType {
+}
