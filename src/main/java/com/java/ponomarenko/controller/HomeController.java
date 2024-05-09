087ca521838b0a5cecc70b5f0a5cc7dd254652e9
Grigorii <osagrigoriy@mail.ru>
2024-05-09 19:33:21 +0400

    test

--- src/main/java/com/java/ponomarenko/controller/HomeController.java
+++ src/main/java/com/java/ponomarenko/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String homePage() {
         return "homePage";
     }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
 }
